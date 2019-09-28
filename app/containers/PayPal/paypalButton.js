98f5dec6f72e5dfc16fefb690d96deb707f6f61e
newmohib <newmohib@gmail.com>
2019-09-28 23:49:54 +0600

    paypal library change

--- app/containers/PayPal/paypalButton.js
+++ app/containers/PayPal/paypalButton.js
@@ -15,45 +15,50 @@ class PaypalButton extends Component {
       //     })
       // });
 
-      console.log("details", details, "data", data);
+      console.log('details', details, 'data', data);
       const detailsInfo = {
-        create_time: "2019-09-23T10:24:50Z",
-        id: "9J397835BN357942C",
+        create_time: '2019-09-23T10:24:50Z',
+        id: '9J397835BN357942C',
         payer: {
-          address: { country_code: "US" },
-          email_address: "mohiburnew@gmail.com",
-          name: { given_name: "Mohibur", surname: "Rahman" },
-          payer_id: "T9R7ZL82AHJ7Y"
+          address: { country_code: 'US' },
+          email_address: 'mohiburnew@gmail.com',
+          name: { given_name: 'Mohibur', surname: 'Rahman' },
+          payer_id: 'T9R7ZL82AHJ7Y',
         },
-        purchase_units: [{
-          amount: { value: '1.00', currency_code: 'USD' },
-          payee: {
+        purchase_units: [
+          {
+            amount: { value: '1.00', currency_code: 'USD' },
+            payee: {
               email_address: 'sb-8ljm1222036@business.example.com',
               merchant_id: 'HGGVB3HQUFP9G',
             },
-          shipping: {
-            address: {
+            shipping: {
+              address: {
                 address_line_1: '1 Main St',
                 admin_area_1: 'CA',
                 admin_area_2: 'San Jose',
                 country_code: 'US',
                 postal_code: '95131',
               },
-            name: { full_name: 'Mohibur Rahman' },
+              name: { full_name: 'Mohibur Rahman' },
+            },
           },
-        }],
-        status: "COMPLETED",
-        update_time: "2019-09-23T10:24:50Z",
-      }
-      let dataInfo = { orderID: "9J397835BN357942C", payerID: "T9R7ZL82AHJ7Y" };
-    }
-    
-    const paymentFailure = (err) => {
+        ],
+        status: 'COMPLETED',
+        update_time: '2019-09-23T10:24:50Z',
+      };
+      const dataInfo = {
+        orderID: '9J397835BN357942C',
+        payerID: 'T9R7ZL82AHJ7Y',
+      };
+    };
+
+    const paymentFailure = err => {
       console.log(err);
-    }
+    };
     const onCancel = () => {
-      console.log("cancel");
-    }
+      console.log('cancel');
+    };
 
     return (
       <div>
@@ -72,7 +77,7 @@ class PaypalButton extends Component {
             sandbox: paypalClientId,
             production: paypalClientId,
           }}
-          currency='USD'
+          currency="USD"
           total="1"
           onError={paymentFailure}
           onSuccess={paymentSuccess}
